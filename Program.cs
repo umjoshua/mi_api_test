ee597a72b2b9951eaded3254935754ab2ae3f17c
Joshua Mathew <umjoshua@gmail.com>
2024-04-09 08:21:06 +0530

    test

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddAzureAppConfiguration(options => 
+    options.Connect(
+        new Uri(builder.Configuration["AppConfig:Endpoint"]),
+        new ManagedIdentityCredential("65a163b7-b95f-4c91-a9f7-0dbb153a8483")
+    )
+);
 
 var app = builder.Build();
 
@@ -16,24 +23,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+    var forecast = app.Configuration["mitest"];
     return forecast;
 })
 .WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/weatherforecast1", () =>
+{
+    var forecast = app.Configuration["test"];
+    return forecast;
+})
+.WithName("GetWeatherForecast1")
 .WithOpenApi();
 
 app.Run();
